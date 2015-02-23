1c52ee4de38e4ed1194c6e4fd6b21f04b7dc1931
Steph Reaves <stephanie.reaves@gmail.com>
2015-02-22 19:12:38 -0500

    add call to flickr api and display an image in a dashboard

---
+++ assets/javascripts/flickr.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	apiCall();
+
+});
+
+var apiCall = function() {
+$.ajax({
+	url: 'https://api.flickr.com/services/feeds/photos_public.gne?&format=json&jsoncallback=wootwoot',
+	type: 'GET',
+	dataType: 'jsonp'
+})
+.success(function() {
+	wootwoot()
+	});
+setTimeout(apiCall, 10000)
+}
+
+function wootwoot(data) {
+	var info = data.items
+	$('#dashboard').empty()
+	for(var i = 0; i < 1; i++) {
+		$('#dashboard').append(info[i].title)
+		$('#dashboard').append(info[i].author)
+		$('#dashboard').append('<img src=' + info[i].media.m + '/>')
+	}
+}
